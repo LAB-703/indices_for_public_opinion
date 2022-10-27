e660f8076fde242455e330177b70f9a13d0c7795
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-10-27 19:31:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -176,10 +176,12 @@ indices_selections = st.sidebar.selectbox(
 
 if indices_selections=="CR":
         st.sidebar.latex(r'''
-    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
     \sum_{k=0}^{n-1} ar^k =
     a \left(\frac{1-r^{n}}{1-r}\right)
     ''')
+        code = '''def hello():
+        print("Hello, Streamlit!")'''
+        st.code(code, language='python')
          
 
 #################################################################
