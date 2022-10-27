72446fb78eea3e57fd57da6d29934674f7c326d1
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-10-27 19:00:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ st.markdown(hide_menu, unsafe_allow_html=True)
 #################################################################
 st.sidebar.subheader("📈 indices for public opinion")
 
-uploaded_files = st.sidebar.file_uploader("Choose a CSV file", accept_multiple_files=True)
+uploaded_files = st.sidebar.file_uploader("Choose a CSV file", type=['csv'], accept_multiple_files=True)
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
