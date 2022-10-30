805b3bf956fc1f4225f1ec36d9f1f22df5a93f73
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-10-31 00:40:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -172,7 +172,8 @@ def CR(df,sort_by,k=3):
     CR=np.sum(CR_df['Si'][0:k]).round(3)
     return CR'''
         st.sidebar.code(CR_code, language='python')
-        tab1.write("CR :",CR(df_news, 'COMPANY', 5))
+        tab1.write("CR :")
+        tab1.write(CR(df_news, 'COMPANY', 5))
         
 if indices_selections=="HHI":
         st.sidebar.latex(r'''
