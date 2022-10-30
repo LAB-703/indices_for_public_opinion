2c10c61c848dcf8fa091bd4ce260c85b7499965c
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-10-31 01:46:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -209,7 +209,7 @@ def CR(df,sort_by,k=3):
     tab2.write(CR(df_reply, 'AUTHOR_RE'))
         
 if uploaded_file is not None:
-    sort_by=st.selectbox("select column to apply", df.columns)
+    sort_by=tab3.selectbox("select column to apply", df.columns)
     tab3.write(CR(df, sort_by))
 
 
