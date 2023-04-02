400ead2e74deb9b0caf5842ca85acc0e0c32bbbc
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2023-04-02 20:00:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -312,8 +312,8 @@ def LQ(df, index_i, i, index_j,j, sort_by):
     st.sidebar.code(LQ_code, language='python')
 
     tab1.write('LQ for COMPANY and ENGINE')
-    selected_ENGINE = st.selectbox('select one in ENGINE', df.ENGINE.unique())
-    selected_COMPANY = st.selectbox('select one in COMPANY',df.COMPANY.unique())
+    selected_ENGINE = st.selectbox('select one in ENGINE', df_news.ENGINE.unique())
+    selected_COMPANY = st.selectbox('select one in COMPANY',df_news.COMPANY.unique())
 
     tab1.write(LQ(df_news, 'ENGINE','DAUM', 'COMPANY','M**', 'AUTHOR')[0])
     tab1.write(LQ(df_news, 'ENGINE','DAUM', 'COMPANY','M**', 'AUTHOR')[1])
